59d0f997764a04929386b0c4ce27191f0b39f4b9
tager1199 <32684792+tager1199@users.noreply.github.com>
2020-09-15 16:14:46 +0100

    Get Input
    Get input text into list of strings

--- MattyCon3/Form1.Designer.cs
+++ MattyCon3/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.RunBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Total = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(13, 13);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(378, 396);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "";
+            // 
+            // RunBtn
+            // 
+            this.RunBtn.Location = new System.Drawing.Point(13, 415);
+            this.RunBtn.Name = "RunBtn";
+            this.RunBtn.Size = new System.Drawing.Size(75, 23);
+            this.RunBtn.TabIndex = 1;
+            this.RunBtn.Text = "Run";
+            this.RunBtn.UseVisualStyleBackColor = true;
+            this.RunBtn.Click += new System.EventHandler(this.RunBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total:";
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Location = new System.Drawing.Point(136, 420);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(19, 13);
+            this.Total.TabIndex = 3;
+            this.Total.Text = "£0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(403, 450);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RunBtn);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.Button RunBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Total;
     }
 }
 
--- MattyCon3/Form1.cs
+++ MattyCon3/Form1.cs
@@ -16,5 +16,10 @@ namespace MattyCon3
         {
             InitializeComponent();
         }
+
+        private void RunBtn_Click(object sender, EventArgs e)
+        {
+            string[] lines = Input.Text.Split('\n');
+        }
     }
 }
