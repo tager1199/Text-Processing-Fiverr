5f207209ac279586eae89757d6f221905ee35db8
tager1199 <32684792+tager1199@users.noreply.github.com>
2020-09-16 01:18:17 +0100

    completed + built exe

--- MattyCon3/Form1.cs
+++ MattyCon3/Form1.cs
@@ -19,7 +19,24 @@ namespace MattyCon3
 
         private void RunBtn_Click(object sender, EventArgs e)
         {
+            double profit = 0.0;
+            double total = 0.0;
+            int place = 0;
             string[] lines = Input.Text.Split('\n');
+            foreach (string i in lines)
+            {
+                string[] words = i.Split(' ');
+                
+                foreach (string j in words)
+                {
+                    bool isDouble = Double.TryParse(j, out profit);
+                    bool isInt = Int32.TryParse(j, out place);
+                    if (isDouble && !isInt)
+                        break;
+                }
+                total += profit;
+            }
+            Total.Text = "£" + total.ToString();
         }
     }
 }
